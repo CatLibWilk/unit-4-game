62a2876425804b85a48a138cfb9010d77a7f9148
Andy Wilk <andrewdwilk@gmail.com>
2018-06-12 14:24:15 -0400

    routine code dump

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 // alert("connect test");
+////////////////////objects////////////////////
 var characters = [
     {
         "charactKey": 0,
@@ -33,13 +34,20 @@ var characters = [
         "character_image": "assets/images/sebulba.png"
     }
 ]
+////////////////////variables////////////////////
+var userChar = "";//holds the position info characters[characterIndex] after is selected in the selectUserChar function
+var enemyChar = ""//ditto for selected enemy
+////////////////////booleans////////////////////
+var userSelect = false;
+var enemySelect = false;
 
 function charBuild(){
     for(var i=0; i<characters.length;i++){
         var charBox = $("<div>");
         charBox.attr("class", "col-2 float-left bg-light charBox m-1  p-2 text-center");
         charBox.attr("characterId", characters[i].charactKey);
-        var thumb = $("<img class='img img-thumbnail charImg   mx-auto d-block'>");
+        charBox.attr("Id", characters[i].charactKey);
+        var thumb = $("<img class='img img-thumbnail charImg mx-auto d-block'>");
         thumb.attr("src", characters[i].character_image);
         var charName = characters[i].character_name;
         var health = characters[i].health_points;
@@ -49,3 +57,50 @@ function charBuild(){
     
 }
 charBuild();
+
+$(document).on("click", ".charBox", selectUserChar);
+
+
+function selectUserChar(){
+    if(userSelect === false){
+    var charID = $(this).attr("characterID");
+    // console.log(charID);
+    var charBox = $("<div>");
+        charBox.attr("class", "col float-left bg-warning charBox m-1  p-2 text-center");
+        charBox.attr("userId", characters[charID].charactKey);
+        var thumb = $("<img class='img img-thumbnail charImg mx-auto d-block'>");
+        thumb.attr("src", characters[charID].character_image);
+        var charName = characters[charID].character_name;
+        var health = characters[charID].health_points;
+        charBox.append(charName, thumb, health);
+        $("#chosen-character").append(charBox);
+        userSelect = true;
+
+        /////////remove stable, reprint boxes in enemy select area
+        $("#characters-stable").empty();
+        
+
+           function availablePopulate(){
+
+            for(var i=0; i<characters.length;i++){//////copy of charBuild function but with new destination
+                var charBox = $("<div>");
+                charBox.attr("class", "col-3 float-left bg-danger charBox m-1  p-2 text-center");
+                charBox.attr("characterId", characters[i].charactKey);
+                charBox.attr("Id", characters[i].charactKey);
+                var thumb = $("<img class='img img-thumbnail charImg mx-auto d-block'>");
+                thumb.attr("src", characters[i].character_image);
+                var charName = characters[i].character_name;
+                var health = characters[i].health_points;
+                charBox.append(charName, thumb, health);
+                $("#enemies-available").append(charBox);
+            }
+            
+            var divToHide = "#" + charID;
+            console.log(divToHide);
+            $(divToHide).attr("class", "d-none");
+            userChar = characters[charID];
+
+        }
+        setTimeout(availablePopulate, 500);
+    }
+}
